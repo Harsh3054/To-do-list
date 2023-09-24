bc548246847833aad061938ae696d0e72c1d06fc
Harshal Patil <harshalspatil21@gmail.com>
2023-09-25 00:35:05 +0530

    first commit

---
+++ todo.js
@@ -0,0 +1,44 @@
+const todoList=[];
+
+renderToDoList();
+function renderToDoList(){
+    let todoListHTML='';
+    for(let i=0;i<todoList.length;i++){
+        const todoObject=todoList[i];
+        //const name=todoObject.name;
+        //const dueDate=todoObject.dueDate;
+        const {name,dueDate}=todoObject;
+        const html=`
+        <div>${name}</div>
+        <div>${dueDate}</div>     
+        <button onclick="
+            todoList.splice(${i},1);
+            renderToDoList();" class="delete-todo-button">Delete
+        </button>`;
+        
+        todoListHTML+=html;
+    }
+    
+    document.querySelector('.js-todo-list').innerHTML=todoListHTML;
+}
+
+
+
+function addToDo(){
+    const inputElement=document.querySelector('.js-name-input');
+    const name=inputElement.value;
+
+    const dateInputElement=document.querySelector('.js-due-date-input');
+
+    const dueDate=dateInputElement.value;
+
+    todoList.push({
+        //name:name,
+        //dueDate:dueDate,
+        name,
+        dueDate
+    });
+    
+    inputElement.value='';
+    renderToDoList();
+}
